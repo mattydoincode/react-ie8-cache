9072013772ee0170d5aafae78d563ed4a1bc09c5
Matt Dean <matt.thomas.dean@gmail.com>
2015-12-09 18:42:44 -0500

    ugh i suck

--- src/detectIE8.js
+++ src/detectIE8.js
@@ -1,9 +1,5 @@
-let isNode = false;
 let result = false;
-if (typeof module !== 'undefined' && module.exports) {
-  isNode = true;
-}
-if (!isNode && typeof document !== undefined) {
+if (typeof document !== 'undefined') {
   const div = document.createElement('div');
   div.innerHTML = '<!--[if lt IE 9]><i></i><![endif]-->';
   result = (div.getElementsByTagName('i').length === 1);
